69c39227adb428f5d4fe7aa706cda92e34738c26
Zinam Mohamed <pk.x3311@gmail.com>
2024-07-03 02:21:26 +0300

    add react-router-dom package & create pages routes on App.jsx file

--- frontend/tasks_managment/src/App.jsx
+++ frontend/tasks_managment/src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
-
+import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'; 
 import Home from "./pages/Home/home"
+import Login from './pages/Login/login';
+import Signup from './pages/Signup/signup';
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path='/dashboard' exact element={<Home />} />
+      <Route path='/login' exact element={<Login />} />
+      <Route path='/signup' exact element={<Signup />} />
+    </Routes>
+  </Router>
+);
 
 const App = () => {
-  return (
-    <div>
-      <Home />
-    </div>
-  )
-}
+  return <div>{routes}</div>
+  
+};
 
-export default App
+export default App;
