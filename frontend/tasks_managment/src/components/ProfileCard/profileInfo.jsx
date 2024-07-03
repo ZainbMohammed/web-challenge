4abb77b7bd0ed97ec928825714d5cd6391790f49
Zinam Mohamed <pk.x3311@gmail.com>
2024-07-03 06:18:38 +0300

    start dashboard page : profile info card done

---
+++ frontend/tasks_managment/src/components/ProfileCard/profileInfo.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { initName } from '../../utils/helper'
+
+const ProfileInfo = ({logoutHandler}) => {
+    
+    
+    return (
+        <div className='flex items-center gap-3'>
+            <div className='w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-semibold bg-slate-100'>
+                {initName('زينب محمد')}
+            </div>
+
+            <div>
+                <p className='text-sm font-medium'>زينب محمد</p>
+                <button className='text-sm text-slate-700 underline pt-2 hover:bg-primary' onClick={logoutHandler}>تسجيل الخروج</button>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default ProfileInfo
--- frontend/tasks_managment/src/pages/Home/home.jsx
+++ frontend/tasks_managment/src/pages/Home/home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/navbar'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  return <>
+    <Navbar/>
+  </>
 }
 
 export default Home
