76db8b671766f50cf25748738655b4a1590a2f6d
Zinam Mohamed <pk.x3311@gmail.com>
2024-07-04 01:43:56 +0300

    design Task Card component

--- frontend/tasks_managment/src/pages/Home/home.jsx
+++ frontend/tasks_managment/src/pages/Home/home.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/navbar'
+import TaskCard from '../../components/TaskCard/taskCard'
+import { MdAdd} from 'react-icons/md'
 
 const Home = () => {
   return <>
-    <Navbar/>
+    <Navbar />
 
-    
+    <div className='container mx-auto'>
+      <div className='grid grid-cols-3 gap-4 mt-8'>
+
+        <TaskCard
+          title='مقابلة عمل الساعة 8 صباحاً'
+          date='9 July 2024'
+          content='مقابلة عمل في شركة بوتفاي لوظيفه مبرمج تطبيقات الموبايل باستخدام فلتر'
+          isPinned={true}
+          onEdit={() => { }}
+          onDelete={() => { }}
+          onPined={() => { }}
+        />
+
+      </div>
+    </div>
+    <button className='w-10 h-10 flex items-center justify-center rounded-xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10 ' onClick={() => {}}>
+      <MdAdd className='text-[32px] text-white font-thin'/>
+    </button>
   </>
 }
 
