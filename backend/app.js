0689452614fd654709dd7e7bf7b5401138b49dd1
Zinam Mohamed <pk.x3311@gmail.com>
2024-07-05 05:54:02 +0300

    perform edit exist task  endpoint => has issue ):

--- backend/app.js
+++ backend/app.js
@@ -1,154 +1,199 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const config = require('./config.json');
-const mongoose = require('mongoose');
+const config = require("./config.json");
+const mongoose = require("mongoose");
 
 mongoose.connect(config.connectionString);
 
-const User = require('./Models/user.model.js');
-const Task = require('./Models/task.model');
+const User = require("./Models/user.model.js");
+const Task = require("./Models/task.model");
 
-
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 
 app.use(
-    cors({
-        origin: '*',
-    })
+  cors({
+    origin: "*",
+  })
 );
 
-const jwt = require('jsonwebtoken');
-const authenticateToken = require('./utilities');
+const jwt = require("jsonwebtoken");
+const authenticateToken = require("./utilities");
 
-app.get('/',(req,res) => {
-    res.json({data:'hello'});
+app.get("/", (req, res) => {
+  res.json({ data: "hello" });
 });
 
 // register new user
-app.post('/register', async (req,res) => {
-    try{
-        const {fullName, email,password} = req.body;
+app.post("/register", async (req, res) => {
+  try {
+    const { fullName, email, password } = req.body;
 
-    if (!fullName){
-        return res.status(400).json( {error: true,message: 'Name is required'} )
+    if (!fullName) {
+      return res.status(400).json({ error: true, message: "Name is required" });
     }
 
-    if (!email){
-        return res.status(400).json( {error: true,message: 'Email is required'} )
+    if (!email) {
+      return res
+        .status(400)
+        .json({ error: true, message: "Email is required" });
     }
 
-    if (!password){
-        return res.status(400).json( {error: true,message: 'Password is required'} )
+    if (!password) {
+      return res
+        .status(400)
+        .json({ error: true, message: "Password is required" });
     }
 
-    const isUserExist = await User.findOne({email:email});
+    const isUserExist = await User.findOne({ email: email });
 
-    if(isUserExist){
-        return res.json({error: true,message: 'User already exist'})
+    if (isUserExist) {
+      return res.json({ error: true, message: "User already exist" });
     }
 
     const user = new User({
-        fullName,
-        email,
-        password
+      fullName,
+      email,
+      password,
     });
     // insert new user
     await user.save();
 
-    const accessToken = jwt.sign({user} , process.env.ACCESS_SECRET_KEY,{
-        expiresIn:'30m',
+    const accessToken = jwt.sign({ user }, process.env.ACCESS_SECRET_KEY, {
+      expiresIn: "30m",
     });
     return res.json({
-        error: false,
-        user,
-        accessToken,
-        message: 'Registration Successful'
+      error: false,
+      user,
+      accessToken,
+      message: "Registration Successful",
+    });
+  } catch (error) {
+    return res.json({ error: true, message: error.message });
+  }
+});
+//login
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email is required" });
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required" });
+  }
+
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  if (user.email == email && user.password == password) {
+    const loginedUser = { user: user };
+    const accessToken = jwt.sign(loginedUser, process.env.ACCESS_SECRET_KEY, {
+      expiresIn: "36000m",
     });
 
-    }catch(error){
-        return res.json({error: true,message: error.message})
-
-    }
-
-})
-//login
-app.post('/login', async (req,res) => {
-    const {email,password} = req.body;
-
-    if (!email){
-        return res.status(400).json( {error: true,message: 'Email is required'} )
-    }
-
-    if (!password){
-        return res.status(400).json( {error: true,message: 'Password is required'} )
-    }
-
-    const user = await User.findOne({email:email});
-
-    if(!user) {
-        return res.status(400).json({message: 'User not found'})
-    }
-
-    if(user.email == email && user.password ==password){
-        const loginedUser = {user:user};
-        const accessToken = jwt.sign(loginedUser,process.env.ACCESS_SECRET_KEY,{
-            expiresIn:'36000m',
-
-        });
-
-        return res.json({
-            error:false,    
-            message: 'Login Successful',
-            email,
-            accessToken,    
-        })
-    }else{
-        return res.status(400).json({
-            error: true,
-            message: 'Invalid Credentials',
-        })
-    }
+    return res.json({
+      error: false,
+      message: "Login Successful",
+      email,
+      accessToken,
+    });
+  } else {
+    return res.status(400).json({
+      error: true,
+      message: "Invalid Credentials",
+    });
+  }
 });
 
 // add new task
-app.post('/add-task',authenticateToken,async (req,res) => {
+app.post("/add-task", authenticateToken, async (req, res) => {
+  const { title, details } = req.body;
+  const { user } = req;
 
-    const {title, details} = req.body;
-    const {user} = req;
+  if (!title) {
+    return res.status(400).json({ error: true, message: "Title is required" });
+  }
 
-    if (!title){
-        return res.status(400).json({error:true, message: 'Title is required'});
+  if (!details) {
+    return res.status(400).json({ error: true, message: "Title is required" });
+  }
+
+  try {
+    const task = new Task({
+      title,
+      details,
+      userId: user._id,
+    });
+
+    await task.save();
+
+    return res.json({
+      error: false,
+      task,
+      message: "task added successful",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: `Internal server error ${error.message}`,
+    });
+  }
+});
+
+// edit exist task
+app.put("/edit-task", authenticateToken, async (req, res) => {
+    console.log( req.params.taskId);
+    console.log( req.body);
+    console.log( "================");
+  const taskId = req.params.taskId;
+  const { title, details, isPinned } = req.body;
+  const { user } = req.user;
+
+  if (!title && !details) {
+    return res
+      .status(400)
+      .json({ error: true, message: "There is no changes" });
+  }
+
+  try {
+    const task = await Task.findOne({ _id: taskId, userId: user._id });
+
+    if (!task) {
+      return res.status(404).json({ error: true, message: "Task not found" });
     }
+    if (title) task.title = title;
+    if (details) task.details = details;
+    if (isPinned) task.isPinned = isPinned;
 
-    if (!details){
-        return res.status(400).json({error:true, message: 'Title is required'});
+    await task.save();
+
+    return res.json({ error: false, task, message: "Task Edited Successful" });
+
+  } catch (error) {
+    if (error.name === 'CastError') {
+        return res.status(400).json({ error: true, message: "Invalid task ID" });
     }
+    // Handle other specific errors here
+    return res.status(500).json({ error: true, message: `Internal Server Error: ${error.message}` });
+}
+});
 
-    try{
-        const task = new Task({
-            title,
-            details,
-            userId: user._id,
-        });
-
-        await task.save();
-
-        return res.json({
-            error:false,
-            task,
-            message: 'task added successful'
-        });
-    }catch(error){
-        return res.status(500).json({
-            error:true,
-            message: `Internal server error ${error.message}`,
-        });
-    }
-})
+// for show all tasks for testing
+app.get("/tasks", async (req, res) => {
+  // get all courses from mongodb using Course model
+  const tasks = await Task.find();
+  res.json({ error: false, data: { tasks } });
+});
 app.listen(8000);
 
 module.exports = app;
